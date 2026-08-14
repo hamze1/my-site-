ebf7be081320dc859fe511f6fe3d665a7f177658
hamze1 <62861301+hamze1@users.noreply.github.com>
2026-08-14 07:52:22 +0330

    Add files via upload

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kyanai-app-v2";
+const CACHE_NAME = "kyanai-app-v3";
 const ASSETS = ["./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", (event) => {
